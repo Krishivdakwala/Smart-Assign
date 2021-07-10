38b0ec01a9d64b7f3122321c20fc361cf98ef0db
vish182 <vishwajitds@gmail.com>
2021-07-10 13:28:58 +0530

    get files api

--- src/api/index.js
+++ src/api/index.js
@@ -62,6 +62,38 @@ export const uploadFile = ({ file, assignment, email }) => {
     });
 };
 
+export const getFile = ({ email, assiName }) => {
+  let ref = fireStorage.ref(`${email}/${assiName}.pdf`);
+  //.child(`${email}/${assiName}.pdf`)
+
+  ref
+    .getDownloadURL()
+    .then((url) => {
+      console.log("file fetched: ", url);
+    })
+    .catch((error) => {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case "storage/object-not-found":
+          // File doesn't exist
+          break;
+        case "storage/unauthorized":
+          // User doesn't have permission to access the object
+          break;
+        case "storage/canceled":
+          // User canceled the upload
+          break;
+
+        // ...
+
+        case "storage/unknown":
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    });
+};
+
 export const upDateStudentPoints = async ({ email, points }) => {
   let data = await getStudentData(email);
 
