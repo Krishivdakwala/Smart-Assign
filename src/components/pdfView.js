59c8f28a3acc057785870c274fb81b514c1c89c4
vish182 <vishwajitds@gmail.com>
2021-07-10 20:47:35 +0530

    refactoring

--- src/components/pdfView.js
+++ src/components/pdfView.js
@@ -18,7 +18,7 @@ const PDFView = ({ url }) => {
   // useEffect(() => {
   //   setURL(url);
   // }, []);
-  //HARDCODE SCAM
+  //HARDCODE
   // "https://firebasestorage.googleapis.com/v0/b/auth-double-slash.appspot.com/o/vishwajitds182%40gmail.com%2FTest%20Assi%201.pdf?alt=media&token=78347c7a-ed30-4f61-850e-38d8ff794dd9"
   return (
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
