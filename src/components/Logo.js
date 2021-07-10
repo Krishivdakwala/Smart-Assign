d2fc5f9e33e39638f48d243e4a03bc9a75f8ec45
Soham Kulkarni <mesohamk@gmail.com>
2021-07-10 16:41:32 +0530

    Updated CSS

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Box, makeStyles, withStyles, Typography } from "@material-ui/core";
-
+import "./Logo.css"
 
 function Logo() {
-    const classes = useStyles();
-    return (
-      <div>
-      <CustomColor>
-      <h1 style={{fontSize: "50px"}}>SMART ASSIGN</h1>   
-      <h5>Where students enjoy assignments</h5>
-        </CustomColor>
-      </div>
-    );
-  }
+  const classes = useStyles();
+  return (
+    <div>
+    <CustomColor>
+    <h1 className="wow">SMART <br/> &ensp; ASSIGN</h1><hr/>
+    <p className="describe"><span style={{color: "#24a7ff"}}> Learn</span> with Intent,<span style={{color: "#24a7ff"}}> Strive</span> for Success</p>
+      </CustomColor>
+    </div>
+  );
+}
   
 const useStyles = makeStyles((theme) => ({}));
 const CustomColor = withStyles({
@@ -20,7 +20,6 @@ const CustomColor = withStyles({
     fontSize: 20,
     background: "-webkit-linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     WebkitBackgroundClip: "text",
-    WebkitTextFillColor: "transparent"
   }
 })(Typography);
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Logo from '../components/Logo';
-import Fade from 'react-reveal/Fade';
 import LoginButton from '../components/LoginButton';
 import NavComponent from '../components/NavComponent';
 import './Landing.css';
@@ -8,42 +7,22 @@ import Slide from 'react-reveal/Slide';
 import { Card } from "react-bootstrap";
 
 function Landing() {
-    return (
+  return (
+    <div className="bgheader">
+    <div className="mainDiv">
       <div>
-      <div className = "Header">
-      <Card className = "styles.logo_frame" bsPrefix="logo_frame" >
+        <Logo />
+      </div>  
 
-
-        <Fade top>
-          <Logo />
-        </Fade>
-    
-
-        <br/>
-        <br/>
-
-
-        <Fade bottom>
+      <div>
           <LoginButton />
-        </Fade>
-        </Card>
-
-
-        
       </div>
-      <div className="AboutUsPara">
-      <Slide left>
-        <h2>About Us</h2>
-        <p>Smart assign akakamkdaskdmakdkadmakdmakdmamdakmdakd
-        adalkdadadmadamdmakdmakdmad
-        adkadmakdmakdmakdmakdmad
-        adkadakdmakdmadkmakdmakdamdakdmakda
-        adkamdkamdkamdk</p>
-    </Slide>
-      </div>
+      
+    </div>
+    </div>
 
-      </div>
-    );
-  }
+    
+  );
+}
 
 export default Landing;
